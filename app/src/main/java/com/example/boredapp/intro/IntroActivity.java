3fa356c98ca8a1e98bfac2688a7a960b4e04dfcb
esen <esen@esen.kubanychbekov>
2019-09-01 17:59:25 +0600

    Add ProgressBar to LaunchActivity

--- app/src/main/java/com/example/boredapp/intro/IntroActivity.java
+++ app/src/main/java/com/example/boredapp/intro/IntroActivity.java
@@ -1,25 +1,28 @@
-package com.example.boredapp;
+package com.example.boredapp.intro;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import com.example.boredapp.R;
+import com.example.boredapp.main.IntroPageFragment;
+import com.example.boredapp.main.MainActivity;
 
-import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
-import android.view.View;
+import java.util.ArrayList;
+import java.util.List;
 
 public class IntroActivity extends AppCompatActivity {
 
     private final static String ARG_TITLE_RES_ID = "title_res";
+
+    IntroPagerAdapter adapter;
+    ViewPager pager;
 
     public static void start(Context context){
         Intent intent = new Intent(context, IntroActivity.class);
@@ -31,35 +34,55 @@ public class IntroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
-        ViewPager pager = findViewById(R.id.pager);
-        SectionPagerAdapter adapter = new SectionPagerAdapter(getSupportFragmentManager());
-        pager.setAdapter(adapter);
+        ArrayList<IntroPageConfig> pages = new ArrayList<>();
 
+        pages.add(new IntroPageConfig(
+                R.string.page1,
+                R.drawable.ic_work));
+
+        pages.add(new IntroPageConfig(
+                R.string.page2,
+                R.drawable.ic_work));
+
+        pages.add(new IntroPageConfig(
+                R.string.page3,
+                R.drawable.ic_work));
+
+        pages.add(new IntroPageConfig(
+                R.string.page4,
+                R.drawable.ic_work));
+
+       adapter = new IntroPagerAdapter(getSupportFragmentManager(),pages);
+        pager = findViewById(R.id.pager);
+        pager.setAdapter(adapter);
     }
 
+    public class IntroPagerAdapter extends FragmentPagerAdapter{
 
-    public  static class  SectionPagerAdapter extends FragmentPagerAdapter {
+        List<IntroPageConfig> pages;
 
-        public SectionPagerAdapter(FragmentManager fm) {
+        public IntroPagerAdapter(FragmentManager fm, List<IntroPageConfig> pages) {
             super(fm);
+            this.pages = pages;
         }
 
         @Override
         public Fragment getItem(int position) {
-            return newInstance(position);
+            return IntroPageFragment.newInstance(pages.get(position));
         }
 
         @Override
         public int getCount() {
-            return 3;
+            return pages.size();
         }
+    }
 
-        public static Fragment newInstance(@StringRes int titleRes){
-            Bundle args = new Bundle();
-            args.putInt(ARG_TITLE_RES_ID,titleRes);
-            IntroFragment fragment = new IntroFragment();
-            fragment.setArguments(args);
-            return fragment;
+    private void nextClick(){
+        if (adapter.getCount() - 1 >= pager.getCurrentItem()) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            pager.setCurrentItem(pager.getCurrentItem() + 1);
         }
     }
 
